f8660dc733b3810caa2f4ad970fb9cec1400e63a
Melih Yıldız <mzyil12@gmail.com>
2024-11-04 14:10:19 +0000

    oopsie

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,7 +33,7 @@ export default [{
 
         parserOptions: {
             project: "tsconfig.json",
-            tsconfigRootDir: "/home/melihy/work/injectjq",
+            tsconfigRootDir: __dirname,
         },
     },
 
